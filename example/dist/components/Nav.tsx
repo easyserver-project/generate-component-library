729a2a88290291e5de0d236b0455f17c87f48c1d
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2021-07-14 00:18:33 +0200

    Wip

--- example/dist/components/Nav.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Nav = ({ children }: { children: JSX.Element[] | JSX.Element }) => {
-  return <ul>{Array.isArray(children) ? children.map((el) => <li>{el}</li>) : { children }}</ul>
-}
